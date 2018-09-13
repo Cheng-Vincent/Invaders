7c847506dcf67290842fbe5d70604151bdde9cf1
Cheng-Vincent <43235182+Cheng-Vincent@users.noreply.github.com>
2018-09-13 02:02:14 -0700

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,430 @@
+GA.custom = function(ga) {
+    ga.move = function(sprites) {
+    if (sprites instanceof Array === false) {
+      internal_move(sprites)
+    } else {
+      for (var i = 0; i < sprites.length; i++) {
+        internal_move(sprites[i])
+      }
+    }
+  };
+    
+    function internal_move(sprite) {
+    sprite.x += sprite.vx | 0;
+    sprite.y += sprite.vy | 0;
+  }
+    
+    ga.randomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+    
+    ga.shoot = function(
+      shooter, angle, offsetFromCenter,
+      bulletSpeed, bulletArray, bulletSprite) {
+    //Make a new sprite using the user-supplied `bulletSprite` function
+    var bullet = bulletSprite();
+
+    //Set the bullet's start point
+    bullet.x
+      = shooter.centerX - bullet.halfWidth
+      + (offsetFromCenter * Math.cos(angle));
+    bullet.y
+      = shooter.centerY - bullet.halfHeight
+      + (offsetFromCenter * Math.sin(angle));
+
+    //Set the bullet's velocity
+    bullet.vx = Math.cos(angle) * bulletSpeed;
+    bullet.vy = Math.sin(angle) * bulletSpeed;
+
+    //Push the bullet into the
+    bulletArray.push(bullet);
+    return bullet;
+  };
+    
+    ga.contain = function(s, bounds, bounce, extra){
+
+    var x = bounds.x,
+        y = bounds.y,
+        width = bounds.width,
+        height = bounds.height;
+
+    //Set `bounce` to `false` by default
+    bounce = bounce || false;
+
+    //The `collision` object is used to store which
+    //side of the containing rectangle the sprite hits
+    var collision;
+
+    //Left
+    if (s.x < x) {
+
+      //Bounce the sprite if `bounce` is true
+      if (bounce) s.vx *= -1;
+
+      //If the sprite has `mass`, let the mass
+      //affect the sprite's velocity
+      if(s.mass) s.vx /= s.mass;
+      s.x = x;
+      collision = "left";
+    }
+
+    //Top
+    if (s.y < y) {
+      if (bounce) s.vy *= -1;
+      if(s.mass) s.vy /= s.mass;
+      s.y = y;
+      collision = "top";
+    }
+
+    //Right
+    if (s.x + s.width > width) {
+      if (bounce) s.vx *= -1;
+      if(s.mass) s.vx /= s.mass;
+      s.x = width - s.width;
+      collision = "right";
+    }
+
+    //Bottom
+    if (s.y + s.height > height) {
+      if (bounce) s.vy *= -1;
+      if(s.mass) s.vy /= s.mass;
+      s.y = height - s.height;
+      collision = "bottom";
+    }
+
+    //The `extra` function runs if there was a collision
+    //and `extra` has been defined
+    if (collision && extra) extra(collision);
+
+    //Return the `collision` object
+    return collision;
+  };
+    
+    ga.hitTestCircle = function(c1, c2, global) {
+    var vx, vy, magnitude, totalRadii, hit;
+
+    //Set `global` to a default value of `false`
+    if(global === undefined) global = false;
+
+    //Calculate the vector between the circles’ center points
+    if(global) {
+
+      //Use global coordinates
+      vx = (c2.gx + c2.radius) - (c1.gx + c1.radius);
+      vy = (c2.gy + c2.radius) - (c1.gy + c1.radius);
+    } else {
+
+      //Use local coordinates
+      vx = c2.centerX - c1.centerX;
+      vy = c2.centerY - c1.centerY;
+    }
+
+    //Find the distance between the circles by calculating
+    //the vector's magnitude (how long the vector is)
+    magnitude = Math.sqrt(vx * vx + vy * vy);
+
+    //Add together the circles' total radii
+    totalRadii = c1.radius + c2.radius;
+
+    //Set hit to true if the distance between the circles is
+    //less than their totalRadii
+    hit = magnitude < totalRadii;
+
+    //`hit` will be either `true` or `false`
+    return hit;
+  };
+    
+    ga.hitTestRectangle = function(r1, r2, global) {
+    var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+    //Set `global` to a default value of `false`
+    if(global === undefined) global = false;
+
+    //A variable to determine whether there's a collision
+    hit = false;
+
+    //Calculate the distance vector
+    if (global) {
+      vx = (r1.gx + r1.halfWidth) - (r2.gx + r2.halfWidth);
+      vy = (r1.gy + r1.halfHeight) - (r2.gy + r2.halfHeight);
+    } else {
+      vx = r1.centerX - r2.centerX;
+      vy = r1.centerY - r2.centerY;
+    }
+
+    //Figure out the combined half-widths and half-heights
+    combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+    combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+    //Check for a collision on the x axis
+    if (Math.abs(vx) < combinedHalfWidths) {
+
+      //A collision might be occuring. Check for a collision on the y axis
+      if (Math.abs(vy) < combinedHalfHeights) {
+
+        //There's definitely a collision happening
+        hit = true;
+      } else {
+
+        //There's no collision on the y axis
+        hit = false;
+      }
+    } else {
+
+      //There's no collision on the x axis
+      hit = false;
+    }
+
+    //`hit` will be either `true` or `false`
+    return hit;
+  };
+
+    ga.hitTestCircleRectangle = function(c1, r1, global) {
+
+    var region, collision, c1x, c1y, r1x, r1y;
+    
+    //Set `global` to a default value of `false`
+    if(global === undefined) global = false;
+
+    //Use either global or local coordinates
+    if (global) {
+      c1x = c1.gx;
+      c1y = c1.gy
+      r1x = r1.gx;
+      r1y = r1.gy;
+    } else {
+      c1x = c1.x;
+      c1y = c1.y
+      r1x = r1.x;
+      r1y = r1.y;
+    }
+
+    //Is the circle above the rectangle's top edge?
+    if (c1y < r1y - r1.halfHeight) {
+
+      //If it is, we need to check whether it's in the 
+      //top left, top center or top right
+      //(Increasing the size of the region by 2 pixels slightly weights
+      //the text in favor of a rectangle vs. rectangle collision test.
+      //This gives a more natural looking result with corner collisions
+      //when physics is added)
+      if(c1x < r1x - 1 - r1.halfWidth) {
+        region = "topLeft";
+      }
+      else if (c1x > r1x + 1 + r1.halfWidth) {
+        region = "topRight";
+      }
+      else {
+        region = "topMiddle";
+      }
+    }
+
+    //The circle isn't above the top edge, so it might be
+    //below the bottom edge
+    else if (c1y > r1y + r1.halfHeight) {
+
+      //If it is, we need to check whether it's in the bottom left,
+      //bottom center, or bottom right
+      if (c1x < r1x - 1 - r1.halfWidth) {
+        region = "bottomLeft";
+      }
+      else if (c1x > r1x + 1 + r1.halfWidth) {
+        region = "bottomRight";
+      }
+      else {
+        region = "bottomMiddle";
+      }
+    }
+
+    //The circle isn't above the top edge or below the bottom edge,
+    //so it must be on the left or right side
+    else {
+      if (c1x < r1x - r1.halfWidth) {
+        region = "leftMiddle";
+      }
+      else {
+        region = "rightMiddle";
+      }
+    }
+
+    //Is this the circle touching the flat sides
+    //of the rectangle?
+    if (region === "topMiddle"
+    || region === "bottomMiddle"
+    || region === "leftMiddle"
+    || region === "rightMiddle") {
+
+      //Yes, it is, so do a standard rectangle vs. rectangle collision test
+      collision = ga.hitTestRectangle(c1, r1, global);  
+    } 
+
+    //The circle is touching one of the corners, so do a
+    //circle vs. point collision test
+    else {
+      var point = {};
+
+      switch (region) {
+        case "topLeft": 
+          point.x = r1x;
+          point.y = r1y;
+          break;
+        
+        case "topRight":
+          point.x = r1x + r1.width;
+          point.y = r1y;
+          break;
+
+        case "bottomLeft":
+          point.x = r1x;
+          point.y = r1y + r1.height;
+          break;
+
+        case "bottomRight":
+          point.x = r1x + r1.width;
+          point.y = r1y + r1.height;
+      }
+      
+      //Check for a collision between the circle and the point
+      collision = ga.hitTestCirclePoint(c1, point, global);
+    }
+
+    //Return the result of the collision.
+    //The return value will be `undefined` if there's no collision
+    if (collision) {
+      return region;
+    } else {
+      return collision;
+    }
+  }; 
+    
+    ga.hitTestCirclePoint = function(c1, point, global) {
+    
+    //Set `global` to a default value of `false`
+    if(global === undefined) global = false;
+
+    //A point is just a circle with a diameter of
+    //1 pixel, so we can cheat. All we need to do is an ordinary circle vs. circle
+    //Collision test. Just supply the point with the properties
+    //it needs
+    point.diameter = 1;
+    point.radius = 0.5;
+    point.centerX = point.x;
+    point.centerY = point.y;
+    point.gx = point.x;
+    point.gy = point.y;
+    return ga.hitTestCircle(c1, point, global); 
+  };
+    
+    ga.rectangleCollision = function(r1, r2, bounce, global) {
+    var collision, combinedHalfWidths, combinedHalfHeights,
+        overlapX, overlapY, vx, vy;
+
+    //Set `bounce` to a default value of `true`
+    if(bounce === undefined) bounce = false;
+
+    //Set `global` to a default value of `false`
+    if(global === undefined) global = false;
+
+    //Calculate the distance vector
+    if(global) {
+      vx = (r1.gx + r1.halfWidth) - (r2.gx + r2.halfWidth);
+      vy = (r1.gy + r1.halfHeight) - (r2.gy + r2.halfHeight);
+    } else {
+      vx = r1.centerX - r2.centerX;
+      vy = r1.centerY - r2.centerY;
+    }
+
+    //Figure out the combined half-widths and half-heights
+    combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+    combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+    //Check whether vx is less than the combined half widths
+    if (Math.abs(vx) < combinedHalfWidths) {
+
+      //A collision might be occurring!
+      //Check whether vy is less than the combined half heights
+      if (Math.abs(vy) < combinedHalfHeights) {
+
+        //A collision has occurred! This is good!
+        //Find out the size of the overlap on both the X and Y axes
+        overlapX = combinedHalfWidths - Math.abs(vx);
+        overlapY = combinedHalfHeights - Math.abs(vy);
+
+        //The collision has occurred on the axis with the
+        //*smallest* amount of overlap. Let's figure out which
+        //axis that is
+
+        if (overlapX >= overlapY) {
+
+          //The collision is happening on the X axis
+          //But on which side? vy can tell us
+          if (vy > 0) {
+            collision = "top";
+
+            //Move the rectangle out of the collision
+            r1.y = r1.y + overlapY;
+          } else {
+            collision = "bottom";
+
+            //Move the rectangle out of the collision
+            r1.y = r1.y - overlapY;
+          }
+          //Bounce
+          if (bounce) {
+            r1.vy *= -1;
+
+            /*Alternative
+            //Find the bounce surface's vx and vy properties
+            var s = {};
+            s.vx = r2.x - r2.x + r2.width;
+            s.vy = 0;
+
+            //Bounce r1 off the surface
+            //bounceOffSurface(r1, s);
+            */
+          }
+        } else {
+
+          //The collision is happening on the Y axis
+          //But on which side? vx can tell us
+          if (vx > 0) {
+            collision = "left";
+
+            //Move the rectangle out of the collision
+            r1.x = r1.x + overlapX;
+          } else {
+            collision = "right";
+
+            //Move the rectangle out of the collision
+            r1.x = r1.x - overlapX;
+          }
+
+          //Bounce
+          if (bounce) {
+            r1.vx *= -1;
+
+            /*Alternative
+            //Find the bounce surface's vx and vy properties
+            var s = {};
+            s.vx = 0;
+            s.vy = r2.y - r2.y + r2.height;
+
+            //Bounce r1 off the surface
+            bounceOffSurface(r1, s);
+            */
+          }
+        }
+      } else {
+
+        //No collision
+      }
+    } else {
+
+      //No collision
+    }
+
+    //Return the collision string. it will be either "top", "right",
+    //"bottom", or "left" depening on which side of r1 is touching r2.
+    return collision;
+  }
+};
